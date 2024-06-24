ccb56f41be4b67b9c93343c08ad9b22f28deebde
claudiamorenomurillo <claudiamorenomurillo29@gmail.com>
2024-06-24 19:35:05 +0200

    Create mantenimiento.sh

---
+++ task/mantenimiento.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+V_ADMIN_DIR=/opt/postgres/admin
+ 
+msg "Ejecutando pi_cientifico_bbdd.sh"
+${V_ADMIN_DIR}/pi_cientifico_bbdd.sh
+if [ $? -ne 0 ]; then
+  err "Fallo en la ejecución de pi_cientifico_bbdd.sh"
+ 
+fi
+ 
+msg "Ejecutando pi_cientifico_bbdd_betalle.sh"
+${V_ADMIN_DIR}/pi_cientifico_bbdd_detalle.sh
+if [ $? -ne 0 ]; then
+  err "Fallo en la ejecución de pi_cientifico_bbdd_detalle.sh"
+ 
+fi
+
+msg "Ejecutando pi_cientifico_bbdd_scan.sh"
+${V_ADMIN_DIR}/pi_cientifico_bbdd_scan.sh
+if [ $? -ne 0 ]; then
+  err "Fallo en la ejecución de pi_cientifico_bbdd_scan.sh"
+ 
+fi
+
+msg "Ejecutando pi_cientifico_bbdd_depurado_date.sh"
+${V_ADMIN_DIR}/pi_cientifico_bbdd_depurado_date.sh
+if [ $? -ne 0 ]; then
+  err "Fallo en la ejecución de pi_cientifico_bbdd_depurado_date.sh"
+ 
+fi
+msg "Finalizado"
