8a2db928b1c538fb4b743c826e48d3100209530b
claudiamorenomurillo <claudiamorenomurillo29@gmail.com>
2024-06-25 09:03:08 +0200

    Update mantenimiento.sh

--- automatizacion/roles/setup_scripts/admin/mantenimiento.sh
+++ automatizacion/roles/setup_scripts/admin/mantenimiento.sh
@@ -5,28 +5,28 @@ parametro=${HOSTNAME}
 definir_salida -o pantallanombrada
 
 msg "Ejecutando pi_cientifico_bbdd.sh"
-${V_ADMIN_DIR}/pi_cientifico_bbdd
+${V_ADMIN_DIR}/pi_cientifico_bbdd.sh
 if [ $? -ne 0 ]; then
   err "Fallo en la ejecución de pi_cientifico_bbdd.sh"
 
 fi
 
 msg "Ejecutando pi_cientifico_bbdd_detalle.sh"
-${V_ADMIN_DIR}/pi_cientifico_bbdd_detalle
+${V_ADMIN_DIR}/pi_cientifico_bbdd_detalle.sh
 if [ $? -ne 0 ]; then
   err "Fallo en la ejecución de pi_cientifico_bbdd_detalle.sh"
 
 fi
 
 msg "Ejecutando pi_cientifico_bbdd_scan.sh"
-${V_ADMIN_DIR}/pi_cientifico_bbdd_scan
+${V_ADMIN_DIR}/pi_cientifico_bbdd_scan.sh
 if [ $? -ne 0 ]; then
   err "Fallo en la ejecución de pi_cientifico_bbdd_scan.sh"
 
 fi
 
 msg "Ejecutando pi_cientifico_bbdd_depurado_date.sh"
-${V_ADMIN_DIR}/pi_cientifico_bbdd_depurado_date
+${V_ADMIN_DIR}/pi_cientifico_bbdd_depurado_date.sh
 if [ $? -ne 0 ]; then
   err "Fallo en la ejecución de pi_cientifico_bbdd_depurado_date.sh"
 
